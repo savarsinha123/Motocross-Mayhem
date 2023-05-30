e5740c101663d8abd8601f7ae80c91a550231795
ssinha@caltech.edu <ssinha@caltech.edu>
2023-05-30 11:56:02 +0000

    basic emscripten stuff

--- demo/game.c
+++ demo/game.c
@@ -18,29 +18,27 @@
 const vector_t WINDOW = ((vector_t){.x = 1500, .y = 1000});
 
 typedef struct state {
-    scene_t *scene;
+  scene_t *scene;
 } state_t;
 
 // helper functions
 
 // key handler function
 void on_key(state_t *state, char key, key_event_type_t type, double held_time) {
-    // TODO: key handlers
+  // TODO: key handlers
 }
 
 state_t *emscripten_init() {
-    srand(time(NULL));
-    vector_t min = VEC_ZERO;
-    vector_t max = WINDOW;
-    sdl_init(min, max);
-    sdl_on_key((key_handler_t)on_key);
+  srand(time(NULL));
+  vector_t min = VEC_ZERO;
+  vector_t max = WINDOW;
+  sdl_init(min, max);
+  sdl_on_key((key_handler_t)on_key);
 }
 
-void emscripten_main(state_t *state) {
-    double dt = time_since_last_tick();
-}
+void emscripten_main(state_t *state) { double dt = time_since_last_tick(); }
 
 void emscripten_free(state_t *state) {
-    scene_free(state->scene);
-    free(state);
+  scene_free(state->scene);
+  free(state);
 }
