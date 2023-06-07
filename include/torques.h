0c1e8be457b0cb2249242677b5cc4a920990ed76
ssinha@caltech.edu <ssinha@caltech.edu>
2023-06-07 09:05:47 +0000

    enzo ceLis

--- include/torques.h
+++ include/torques.h
@@ -9,5 +9,3 @@ void create_general_torque(body_t *body, vector_t force,
                            vector_t point_of_evaluation, vector_t pivot);
 
 #endif // #ifndef __TORQUES_H__
-
-#endif // #ifndef __TORQUES_H__
